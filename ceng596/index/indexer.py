fe5b6422b1688ef51e4721f3b43194588d004712
e-hengirmen <ehengirmen@hotmail.com>
2024-06-05 22:28:48 +0300

    added txt files

--- ceng596/index/indexer.py
+++ ceng596/index/indexer.py
@@ -1,8 +1,12 @@
 import os
 import pyterrier as pt
 
-data_dir = os.path.normpath(os.getcwd() + os.sep + os.pardir) + "\\AP_collection\\coll"
-index_dir = os.path.normpath(os.getcwd() + os.sep + os.pardir) + "\\index\\files"
+os.environ["JAVA_HOME"] = "/usr/lib/jvm/java-11-openjdk-amd64"
+# data_dir = os.path.normpath(os.getcwd() + os.sep + os.pardir) + "\\AP_collection\\coll"
+# index_dir = os.path.normpath(os.getcwd() + os.sep + os.pardir) + "\\index\\files"
+
+data_dir = 'AP_collection/coll/'
+index_dir = '/home/ehengirmen/github/ceng596-termproject/ceng596/index/files/'
 
 pt.init()
 # list of filenames to index
@@ -15,3 +19,27 @@ indexref = indexer.index(files)
 # load the index, print the statistics
 index = pt.IndexFactory.of(indexref)
 print(index.getCollectionStatistics().toString())
+
+
+
+tfidf = pt.BatchRetrieve(indexref, wmodel="TF_IDF")
+bm25 = pt.BatchRetrieve(indexref, wmodel="BM25")
+query = "prime minister powerful infrastructure"
+results = bm25.search(query)
+
+print(results[['docno', 'rank', 'score']])
+
+
+# for field in results:
+#     for val in results[field]:
+
+topics = pt.io.read_topics('AP_collection/topics.txt', format='singleline')
+qrels = pt.io.read_qrels('AP_collection/qrels.txt')
+exp = pt.Experiment(
+    [tfidf, bm25],
+    topics,
+    qrels,
+    eval_metrics=["map", "recip_rank"]
+) 
+
+bm25.get_topics(), bm25.get_qrels() kısımlarına doğrudan path versen çalışıyor olmalı
